7331f038cb723d1b0c8da79fda210a7ba3a5b154
Ubuntu <ubuntu@ip-172-31-68-156.ec2.internal>
2021-09-14 02:06:18 +0000

    vriable

---
+++ variable.cpp
@@ -0,0 +1,32 @@
+// Copyright (c) 2021 haokai All rights reserved
+//
+// Created by: Haokai
+// Created on: Sept 2021
+// This is the "circle" program, with proper style
+
+#include <iostream>
+int length;
+int width;
+int area;
+int perimeter;
+
+int main() {
+  // This function calculate the area and perimeter of circle
+
+  // input
+  std::cout << "enter length of the rectangle(mm): " << std::endl;
+  std::cin >> length;
+  std::cout << "enter width of the rectangle(mm): " << std::endl;
+  std::cin >> width;
+
+  // process
+  area = length * width;
+  perimeter = (length + width) * 2;
+
+  // putput
+  std::cout << "" << std::endl;
+  std::cout << "a = L x W = " << area << " mm²" << std::endl;
+  std::cout << "p = (L + W) x 2 = " << perimeter << " mm" << std::endl;
+  std::cout << "" << std::endl;
+  std::cout << "Done" << std::endl;
+}
